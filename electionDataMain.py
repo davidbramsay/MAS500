16e898bbe50187664f831e8472205a420021d32c
David Ramsay <dramsay@mit.edu>
2014-11-12 00:45:08 -0500

    assignment 1

--- electionDataMain.py
+++
@@ -1,7 +0,0 @@
-from electionDataPull import electionDataMain
-
-ElectionResults12 = electionDataMain('http://www.archives.gov/federal-register/electoral-college/2012/popular-vote.html')
-ElectionResults12.printSiteData()
-
-
-
