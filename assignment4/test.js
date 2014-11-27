0658e78fb0de4ac48b59298e496d8e281982c788
David Ramsay <dramsay@mit.edu>
2014-11-27 02:55:32 -0500

    update test func

--- assignment4/test.js
+++ assignment4/test.js
@@ -1,11 +1,17 @@
 var gv = require('./globalvoices.js');
+
 //console.log(gv.returnCountries());
-//console.log(gv.writeAllCountryStoriesToDatabase());
+
+console.log(gv.writeAllCountryStoriesToDatabase());
+
 console.log('-------------------------------------');
+
 //console.log(gv.fullURLfromCountry("Spain"));
+
 //console.log('-------------------------------------');
-gv.recentStoriesFromDatabase("Spain", function(result){
 
-	console.log("finallog" + result);
+/*gv.recentStoriesFromDatabase("Spain", function(result){
 
-});
+	console.log(result);
+
+});*/
